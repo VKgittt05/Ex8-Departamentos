9a34697f99ea973c08a7a25cf70cfbe95470cbe5
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 01:26:28 -0300

    initial commits
    Exercicio 8

---
+++ Departamentos/src/test/java/com/Gerenciamento_Departamentos/Departamentos/DepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciamento_Departamentos.Departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
